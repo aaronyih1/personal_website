ddf5989614138cc42a5600c91afcbce3336fcbe2
Aaron Yih <aaron.yih@ucla.edu>
2016-09-07 18:16:51 -0700

    first draft of enrou page

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ app.get('/contact', function(req, res) {
     res.sendFile( __dirname + "/static/" + "contact.html" );
 });
 
+/*/////////WORK FILE LINKS//////////*/
+app.get('/hi_enrou', function(req, res) {
+    res.sendFile( __dirname + "/static/work_files/enrou/" + "enrou.html" );
+});
+
 app.listen(process.env.PORT || 3000);
