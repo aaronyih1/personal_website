1e7cdca2be4efa1fc988392ca158d797725c2f89
Aaron Yih <aaron.yih@ucla.edu>
2016-09-15 17:10:16 -0700

    added enrou and boxcamp links to front page

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.use(express.static('static'));
 app.use('/enrou', express.static('static/work_files/enrou'));
+app.use('/enrou', express.static('static/work_files/boxcamp'));
 
 app.get('/contact', function(req, res) {
     res.sendFile( __dirname + "/static/" + "contact.html" );
@@ -12,5 +13,9 @@ app.get('/contact', function(req, res) {
 app.get('/hi_enrou', function(req, res) {
     res.sendFile( __dirname + "/static/work_files/enrou/" + "enrou.html" );
 });
+app.get('/boxcamp', function(req, res) {
+    res.sendFile( __dirname + "/static/work_files/boxcamp/" + "boxcamp.html" );
+});
+
 
 app.listen(process.env.PORT || 3000);
